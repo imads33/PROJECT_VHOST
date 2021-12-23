7e94e5a40decdca5d917f488b67fc15b152c4c12
imads33 <imadsab33ayu@gmail.com>
2021-12-23 23:31:54 +0530

    COMMIT

---
+++ accounts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+# from contacts.models import Contacts
+
+# Create your views here.
