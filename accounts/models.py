180a107f8202b2c057453f6e56c4e2a8c0837924
imads33 <imadsab33ayu@gmail.com>
2021-12-25 23:38:57 +0530

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from accounts.views import register
+
 # Create your models here.
+
+
+class Accounts(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
+    email = models.EmailField()
+    password_1 = models.CharField(max_length=50)
+    reg_date = models.DateField()
--- domaininfo/models.py
+++ domaininfo/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from accounts.models import Accounts
+
+from accounts.views import register
+from datetime import date
 
 # Create your models here.
+
+
+class Domain(models.Model):
+    domain_name = models.CharField(max_length=100)
+    reg_date = models.DateField(default=date.today)
+    planStartDate = models.DateField(default=date.today)
+    planEndDate = planStartDate - models.DateField()
+    app_name = models.Choices(max_length=200)
+    user_name = models.ForeignKey(Accounts)
+
+
+class Plan(models.Model):
+    plan_type = models.CharField(max_length=50)
+    plan_price = models.IntegerField(max_length=50)
+    storage = models.CharField(max_length=50)
+
+
+class Server(models.Model):
+    server_loc = models.CharField(max_length=100)
+    storage = models.ForeignKey(Plan, on_delete=models.CASCADE,)
+    domain_name = models.ForeignKey(Domain, on_delete=models.CASCADE,)
+    user_name = models.ForeignKey(Accounts)
