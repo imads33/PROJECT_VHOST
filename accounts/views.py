9246c25413d96b01a7d07ccd3b785ba958386bcd
imads33 <imadsab33ayu@gmail.com>
2021-12-25 23:43:13 +0530

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,12 @@ from django.contrib.auth.models import User
 
 
 def register(request):
+    # first_name
+    # last_name
+    # username
+    # email
+    # password_1
+    # reg_date
     return render(request, 'pages/index.html')
 
 
